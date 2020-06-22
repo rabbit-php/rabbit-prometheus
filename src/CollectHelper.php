8eabf5ea15733f8458e32daa7d7fe2a4ff21bb7f
albert <amuluowin@163.com>
2020-06-22 09:33:35 +0800

    fix collectPool

--- src/CollectHelper.php
+++ src/CollectHelper.php
@@ -63,21 +63,23 @@ class CollectHelper
     public static function collectPool(CollectorRegistry $registry, int $workerId): void
     {
         foreach (PoolManager::getPools() as $pool) {
-            $gauge = $registry->getOrRegisterGauge(self::WORKER, "pool_idle", self::HELP, [self::LABLE, 'pool_dsn']);
-            $mgauge = $registry->getOrRegisterGauge(self::WORKER, "pool_num", self::HELP, [self::LABLE, 'pool_dsn']);
-            $current = $registry->getOrRegisterGauge(self::WORKER, "pool_current", self::HELP, [self::LABLE, 'pool_dsn']);
-            $addrList = [];
-            foreach ($pool->getServiceList() as $uri) {
-                $parsed_url = parse_url($uri);
-                $scheme = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
-                $host = isset($parsed_url['host']) ? $parsed_url['host'] : '';
-                $port = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
-                $addrList[] = "$scheme$host$port";
+            if (method_exists($pool, 'getServiceList')) {
+                $addrList = [];
+                foreach ($pool->getServiceList() as $uri) {
+                    $parsed_url = parse_url($uri);
+                    $scheme = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
+                    $host = isset($parsed_url['host']) ? $parsed_url['host'] : '';
+                    $port = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
+                    $addrList[] = "$scheme$host$port";
+                }
+                $mgauge = $registry->getOrRegisterGauge(self::WORKER, "pool_num", self::HELP, [self::LABLE, 'pool_dsn']);
+                $current = $registry->getOrRegisterGauge(self::WORKER, "pool_current", self::HELP, [self::LABLE, 'pool_dsn']);
+                $gauge = $registry->getOrRegisterGauge(self::WORKER, "pool_idle", self::HELP, [self::LABLE, 'pool_dsn']);
+                $labels = [$workerId, implode(',', $addrList)];
+                $gauge->set($pool->getPool()->length(), $labels);
+                $mgauge->set($pool->getPoolConfig()->getMinActive(), $labels);
+                $current->set($pool->getCurrentCount(), $labels);
             }
-            $labels = [$workerId, implode(',', $addrList)];
-            $gauge->set($pool->getPool()->length(), $labels);
-            $mgauge->set($pool->getPoolConfig()->getMinActive(), $labels);
-            $current->set($pool->getCurrentCount(), $labels);
         }
     }
 }
